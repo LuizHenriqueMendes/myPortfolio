3c4e1de6944fdd8aec2bf825e5cd5750a5e9ee72
FeixeDeLuz <luiz.mendescastro@gmail.com>
2024-10-29 15:55:16 -0300

    Update Portfolio

--- my-portfolio/src/Components/Timeline.jsx
+++ my-portfolio/src/Components/Timeline.jsx
@@ -6,31 +6,48 @@ function Timeline() {
   return (
     <div className="timeline">
       <div className="timeline-item">
-        <div className="timeline-year">2020</div>
+        <div className="timeline-year">12/2020</div>
         <div className="timeline-content">
           <div className="timeline-image">
             <img src="image1.jpg" alt="Evento 1" />
           </div>
           <div className="timeline-text">
             <h3>Me formei no Ensino Médio no Colégio Santo Inácio</h3>
-            <p>Descrição do evento 1 que aconteceu em 2020.</p>
+            <p>Entrei no Santo Inácio em 2008, onde estudei durante 13 anos. 
+                Ao longo desse tempo, tive um ótimo desempenho escolar, fiz viagens com a 
+                escola e trabalhos voluntários.
+                 </p>
           </div>
         </div>
       </div>
       
       <div className="timeline-item">
-        <div className="timeline-year">2021</div>
+        <div className="timeline-year">01/2021</div>
         <div className="timeline-content">
           <div className="timeline-image">
             <img src="image2.jpg" alt="Evento 2" />
           </div>
           <div className="timeline-text">
-            <h3>Título do Evento 2</h3>
-            <p>Descrição do evento 2 que aconteceu em 2021.</p>
+            <h3>Entrei para a PUC-Rio</h3>
+            <p>Em 2021 fui aprovado na PUC-Rio no curso de Ciência da Computação,
+            aonde completei o primeiro período da faculdade.</p>
           </div>
         </div>
       </div>
-
+      <div className="timeline-item">
+        <div className="timeline-year">07/2021</div>
+        <div className="timeline-content">
+          <div className="timeline-image">
+            <img src="image2.jpg" alt="Evento 2" />
+          </div>
+          <div className="timeline-text">
+            <h3>Entrei para o CEFET/RJ</h3>
+            <p>Mesmo fazendo curso na PUC, fui aprovado para o CEFET, 
+            então optei por trancar minha matrícula na PUC e dar início ao curso
+            de Ciência da Computação no CEFET/RJ.</p>
+          </div>
+        </div>
+      </div>
       {/* Adicione mais itens conforme necessário */}
     </div>
   );
