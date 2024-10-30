3e0aa2d7190ab7f77c3aada1b99e7f6352ce22f7
FeixeDeLuz <luiz.mendescastro@gmail.com>
2024-10-30 12:15:04 -0300

    Projects Skeleton + Contact Icons

--- my-portfolio/src/Pages/Contact.jsx
+++ my-portfolio/src/Pages/Contact.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 function Contact() {
   return (
-    <div>
-      <h1>Contato</h1>
-      <p>Esta é a página de projetos.</p>
+    <div className="home-container">  
+    <h1 className="welcome-text">Fale comigo!</h1>
+    <div className="home-content">
+      <div className="home-about">
+        <p>Eu me chamo Luiz Henrique Mendes e tenho 21 anos. Atualmente estudo 
+        Ciência da Computação no CEFET-RJ e estou em busca de um estágio!</p>
+    
+        <p>Desde pequeno gostei de jogar video-game, por isso um dia decidi 
+        tentar criar meu próprio jogo. Foi então que descobri a programação. 
+        Comecei a pesquisar mais sobre e encontrei o curso de Ciência da 
+        Computação, nele descobri mais e mais sobre o mundo da programação, 
+        e como ela está presente em quase tudo.</p>
+
+        <p>Hoje em dia sou Estagiário de T.I. na empresa Centauro Engenharia 
+        há 1 ano, mas estou em busca de um novo emprego para evoluir minhas 
+        habilidades em outras partes desta profissão.</p>
+      </div>
     </div>
+  </div>
   );
 }
 
--- my-portfolio/src/Pages/Projects.jsx
+++ my-portfolio/src/Pages/Projects.jsx
@@ -10,8 +10,8 @@ function Projects() {
       <div className="project-item">
         <h2 className="project-name">Scoutball</h2>
         <p className="project-description">
-          Este projeto foi criado para a matéria de Programação de Software para Web, eu e os componentes do grupo 
-          utilizamos React e Javascript. Além disso integramos uma API que informava diversas informações sobre jogadores
+          Este projeto foi criado para a matéria de Programação de Software para Web, utilizando 
+          React e Javascript. Fizemos um site que integra uma API que informa diversas informações sobre jogadores
           de diversos campeonatos diferentes, tanto do Brasil quanto de outros países. 
         </p>
         <div className="project-video">
@@ -30,7 +30,8 @@ function Projects() {
       <div className="project-item">
         <h2 className="project-name">Wow!</h2>
         <p className="project-description">
-          Projeto desenvolvido para
+        Projeto desenvolvido para a disciplina Arquitetura e Padrões de Software, aonde foi criada uma rede social,
+        na qual o usuário pode postar, curtir, comentar e ver postagens de outros usuários.
         </p>
         <div className="project-video">
           <iframe
@@ -45,7 +46,43 @@ function Projects() {
         </div>
       </div>
 
-      {/* Adicione mais projetos conforme necessário */}
+      <div className="project-item">
+        <h2 className="project-name">GalaxyShooter</h2>
+        <p className="project-description">
+          Jogo desenvolvido em um curso da Udemy, utilizando Unity e C#.
+        </p>
+        <div className="project-video">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/VIDEO_ID"
+            title="Video do Projeto 2"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    
+      <div className="project-item">
+        <h2 className="project-name">RunDuckRun</h2>
+        <p className="project-description">
+          Jogo de plataforma que desenvolvi adaptando os conhecimentos adquiridos sobre programação
+          de jogos e na faculdade.
+        </p>
+        <div className="project-video">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/VIDEO_ID"
+            title="Video do Projeto 2"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+
     </div>
   );
 }
