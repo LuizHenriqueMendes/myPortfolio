349d395d4bda18eea01578099a59c5c28f7b0628
FeixeDeLuz <luiz.mendescastro@gmail.com>
2024-11-01 22:06:18 -0300

    Reponsive CSS

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -16,11 +16,11 @@ function Sidebar() {
     <div className="sidebar">
       <h1 className="sidebar-title">Luiz Henrique Mendes</h1>
       <nav className="sidebar-nav">
-        <Link to="/">{t('home')}</Link>
-        <Link to="/about">{t('about')}</Link>
-        <Link to="/projects">{t('projects')}</Link>
-        <Link to="/skills">{t('skills')}</Link>
-        <Link to="/contact">{t('contact')}</Link>
+        <Link to="/" className="sidebar-link">{t('home')}</Link>
+        <Link to="/about" className="sidebar-link">{t('about')}</Link>
+        <Link to="/projects" className="sidebar-link">{t('projects')}</Link>
+        <Link to="/skills" className="sidebar-link">{t('skills')}</Link>
+        <Link to="/contact" className="sidebar-link">{t('contact')}</Link>
       </nav>
       <div className="language-toggle">
         <button className="btn-language" onClick={() => changeLanguage('pt')}><img className="logoBR" src={logoBR} alt="BR Flag"/></button>
